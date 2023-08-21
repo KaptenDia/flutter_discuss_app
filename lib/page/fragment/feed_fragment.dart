3b6cb58e715d42b27aa241e56bbc84a0767fd2e8
KaptenDia <84517697+KaptenDia@users.noreply.github.com>
2023-08-21 14:43:48 +0700

    update feed fragment

--- lib/page/fragment/feed_fragment.dart
+++ lib/page/fragment/feed_fragment.dart
@@ -1,13 +1,60 @@
+import 'dart:convert';
+
 import 'package:d_view/d_view.dart';
+import 'package:discuss_app/controller/c_feed.dart';
+import 'package:discuss_app/controller/c_user.dart';
+import 'package:discuss_app/domain/models/topic.dart';
+import 'package:discuss_app/page/widgets/item_topic.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FeedFragment extends StatelessWidget {
   const FeedFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: DView.textTitle("Feed"),
+    final cUser = context.read<CUser>();
+    if (cUser.data == null) {
+      return DView.loadingCircle();
+    } else {
+      context.read<CFeed>().setTopics(cUser.data!.id);
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+          child: DView.textTitle('Feed', size: 24),
+        ),
+        Expanded(
+          child: Consumer<CFeed>(
+            builder: (contextConsumer, _, child) {
+              if (_.topics.isEmpty) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DView.empty(),
+                    DView.empty('Or you are not following anybody'),
+                  ],
+                );
+              }
+              return ListView.builder(itemBuilder: (context, index) {
+                Topic topic = _.topics[index];
+                List images = jsonDecode(topic.images);
+                return Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    16,
+                    8,
+                    16,
+                    index == _.topics.length - 1 ? 30 : 8,
+                  ),
+                  child: ItemTopic(topic: topic, images: images),
+                );
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
